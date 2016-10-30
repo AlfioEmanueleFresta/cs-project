c9a46fa21aeb8c0f220f6963c8562569524bcccf
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-10-30 17:05:44 +0000

    Updates

--- example.py
+++ example.py
@@ -7,12 +7,7 @@ import datetime
 
 options = {
     'glove_use_cache': [True],
-    'vector_size': [300],        # 50 for loading speed, 300 for accuracy.
-    'train_percentage': [0.7],
-    'max_epochs': [500],
-    'dense_layers': [2],
-    'dense_layers_size': [200],
-    'dense_layers_dropout': [0.3],
+    'vector_size': [100],        # 50 for loading speed, 300 for accuracy.
     'network_class': [LSTMNetwork]
 }
 
@@ -23,23 +18,22 @@ for options in get_options_combinations(options):
 
     vector_size = options['vector_size']
 
-    g = Glove('data/glove.6B.%dd.txt.gz' % vector_size,
+    g = Glove('data/glove.6B.%dd.txt' % vector_size,
               verbose=True, use_cache=options['glove_use_cache'])
     vector_size = g.vector_length
 
     network_class = options['network_class']
     n = network_class(input_features_no=vector_size,
-                      output_categories_no=9,
-                      max_words_per_sentence=15,
-                      dense_layers=options['dense_layers'],
-                      dense_layers_size=options['dense_layers_size'],
-                      dense_layers_dropout=options['dense_layers_dropout'],
-                      train_percentage=options['train_percentage'],
-                      train_max_epochs=options['max_epochs'])
+                      output_categories_no=50,
+                      max_words_per_sentence=100,
+                      train_batch_size=50)
     n.build_network()
     n.load_glove(g)
-    n.load_training_data('data/training.txt')
-    n.train()
+    n.load_training_data('data/trec.txt')
+    try:
+        n.train()
+    except KeyboardInterrupt:
+        print("Training interrupted. Continuing.")
     # n.save("x.npz")
     n.interactive_predict(glove=g)
 
