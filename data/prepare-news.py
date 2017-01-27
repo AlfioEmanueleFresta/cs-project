73f402832b29faa2519ee611401b58dbaf20f9ba
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2017-01-27 16:42:08 +0000

    Prepare TagMyNews dataset script

---
+++ data/prepare-news.py
@@ -0,0 +1,37 @@
+import os
+
+NEWS_FILE = 'raw/en09062011.news'
+
+prepared = {}
+
+with open(NEWS_FILE, 'rt') as f:
+
+    while True:
+
+        title = f.readline().rstrip("\n")
+        subtitle = f.readline().rstrip("\n")
+        url = f.readline().rstrip("\n")
+        id = f.readline().rstrip("\n")
+        date = f.readline().rstrip("\n")
+        source = f.readline().rstrip("\n")
+        category = f.readline().rstrip("\n")
+        f.readline()
+
+        if title == "":
+            break
+
+        sentence = "%s. %s" % (title, subtitle)
+        label = category
+
+        if label in prepared:
+            prepared[label].append(sentence)
+
+        else:
+            prepared[label] = [sentence]
+
+for label in prepared.keys():
+    for sentence in prepared[label]:
+        print("Q: %s" % sentence)
+    print("A: %s" % label)
+    print("")
+print("")
